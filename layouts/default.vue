1f521555b3e3258e29bdec55db98f5765e9f654f
Scott Kennedy <scottyzen@gmail.com>
2021-06-03 22:50:32 +0100

    Scrool lock

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="flex flex-col min-h-screen">
-    <Nuxt class="flex-1" />
+  <div class="min-h-screen pb-8 bg-gray-500">
+    <Nuxt class="min-h-screen pb-16" />
     <ul
-      class="
-        grid grid-cols-4
-        sticky
-        bottom-0
-        w-full
-        justify-between
-        text-center
-      "
+      class="fixed bottom-0 grid justify-between w-full grid-cols-4 text-center"
     >
-      <div class="bg-gray-100 text-sm p-2">HOME</div>
-      <div class="bg-gray-100 text-sm p-2">STOCK</div>
-      <div class="bg-gray-100 text-sm p-2">ORDERS</div>
-      <div class="bg-gray-100 text-sm p-2">MY ACCOUNT</div>
+      <div class="py-4 text-sm bg-gray-100">HOME</div>
+      <div class="py-4 text-sm bg-gray-100">STOCK</div>
+      <div class="py-4 text-sm bg-gray-100">ORDERS</div>
+      <div class="py-4 text-sm bg-gray-100">ACCOUNT</div>
     </ul>
   </div>
 </template>
+
+<style>
+html {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+
+body {
+  height: 100%;
+  padding: 0;
+  overflow: auto;
+  margin: 0;
+  -webkit-overflow-scrolling: touch;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   // darkMode: 'class', // or 'media' or 'class'
   theme: {
     container: {
       center: true,
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.blueGray,
+      red: colors.red,
+      yellow: colors.amber,
+      indigo: colors.indigo,
+      blue: colors.cyan,
+      pink: colors.pink,
+      green: colors.green,
     },
   },
   purge: {
